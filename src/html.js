069e47a7918f2b8cc0b5b6a37a4b6e28a1c00e3e
Emmanuel Fallancy <emmanuel.fallancy@gmail.com>
2018-04-12 02:49:46 +1000

    Add logo component missing from committed and also html

--- src/html.js
+++ src/html.js
@@ -1,24 +1,24 @@
-import React from "react"
+import React from 'react';
 
-let stylesStr
+let stylesStr;
 if (process.env.NODE_ENV === `production`) {
   try {
-    stylesStr = require(`!raw-loader!../public/styles.css`)
+    stylesStr = require(`!raw-loader!../public/styles.css`);
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 }
 
 module.exports = class HTML extends React.Component {
   render() {
-    let css
+    let css;
     if (process.env.NODE_ENV === `production`) {
       css = (
         <style
           id="gatsby-inlined-css"
-          dangerouslySetInnerHTML={{ __html: stylesStr }}
+          dangerouslySetInnerHTML={{__html: stylesStr}}
         />
-      )
+      );
     }
     return (
       <html {...this.props.htmlAttributes} lang="en">
@@ -39,11 +39,11 @@ module.exports = class HTML extends React.Component {
           <div
             key={`body`}
             id="___gatsby"
-            dangerouslySetInnerHTML={{ __html: this.props.body }}
+            dangerouslySetInnerHTML={{__html: this.props.body}}
           />
           {this.props.postBodyComponents}
         </body>
       </html>
-    )
+    );
   }
-}
+};
