e404e8c0c41b60daf5ab5bb19643390ee42e0972
Emmanuel Fallancy <emmanuel.fallancy@gmail.com>
2018-04-13 01:14:34 +1000

    Update README and favicon

--- README.md
+++ README.md
@@ -3,7 +3,10 @@ Hacker News app build with GatsbyJS.
 
 Utilising all around (mostly) Gatsby packages.
 
-_Still in progress but viewable at_ [Gatsby HNPWA](https://gatsbyhn.netlify.com)
+Link to the app can be found at [Gatsby HN](https://gatsbyhn.netlify.com).
+
+## Motivation
+This is built as a mean of demonstrating on building a Progressive Web App (PWA) based on Hacker News as its primary content resource, using [GatsbyJS](https://www.gatsbyjs.org) and powered by [Netlify](https://www.netlify.com).
 
 ## Plugins include
 - `gatsby-plugin-glamor`
--- src/html.js
+++ src/html.js
@@ -29,8 +29,12 @@ module.exports = class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+          <link
+            rel="icon"
+            href="/hn-favicon-16x16.png"
+            type="image/png"
+            sizes="16x16"
+          />
           {this.props.headComponents}
           {css}
         </head>
